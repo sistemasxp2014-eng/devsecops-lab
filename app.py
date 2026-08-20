10da4db02343dd587d7f9788a8f0aaf5da66c7f7
Limberth Condori Choque <sistemasxp2014@gmail.com>
2026-08-20 16:53:12 -0400

    fix: añadir exclusiones de semgrep y bypass para puerto docker

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ def home():
     return "¡Hola Mundo desde un entorno seguro!"
 
 if __name__ == "__main__":
-    # Leer el token de forma segura desde variables de entorno
     TEST_TOKEN = os.getenv("GITHUB_PAT", "token_por_defecto_si_no_existe")
-    app.run(host="0.0.0.0", port=5000)
+    # ✅ nosemgrep: Se requiere 0.0.0.0 para enlazar correctamente dentro del contenedor Docker
+    app.run(host="0.0.0.0", port=8080) # nosemgrep
