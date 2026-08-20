08ef2c89feed5926a02669935cafb6c0c74a637d
Limberth Condori Choque <sistemasxp2014@gmail.com>
2026-08-20 15:00:33 -0400

    fix: remediar vulnerabilidades de app, iac y contenedor

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,8 @@
-FROM python:3.7-slim
+FROM python:3.11-slim
 WORKDIR /app
+RUN useradd -m appuser
 COPY app/ /app/
 RUN pip install --no-cache-dir -r requirements.txt
+USER appuser
 EXPOSE 8080
 CMD ["python", "app.py"]
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,22 +1,22 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_s3_bucket" "bucket_inseguro" {
+resource "aws_s3_bucket" "bucket_seguro" {
   bucket = "mi-bucket-devsecops-demo-12345"
 }
 resource "aws_s3_bucket_public_access_block" "publico" {
-  bucket = aws_s3_bucket.bucket_inseguro.id
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
-  restrict_public_buckets = false
+  bucket = aws_s3_bucket.bucket_seguro.id
+  block_public_acls = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
-resource "aws_security_group" "sg_inseguro" {
-  name = "sg_ssh_abierto"
+resource "aws_security_group" "sg_seguro" {
+  name = "sg_ssh_restringido"
   ingress {
     from_port = 22
     to_port   = 22
     protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["10.0.0.0/16"]
   }
 }
